5cdfd9a2964e44ffab98481acae04816fef75aaa
Ryan Yin <xiaoyin_c@qq.com>
2023-06-16 20:12:22 +0800

    fix: revert to buildLinux to try to avoid error - 'symbol LIGHT_NET depends on KVM' and 'symbol KVM depends on NET'

--- pkgs/kernel.nix
+++ pkgs/kernel.nix
@@ -1,38 +1,38 @@
-# { lib, stdenv, buildLinux, src, ... } @ args:
+{ lib, stdenv, buildLinux, src, ... } @ args:
 
-# # args of buildLinux:
-# #   https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/kernel/generic.nix
-# buildLinux (args // {
-#   # check version in kernel source tree's Makefile
-#   #   https://github.com/revyos/thead-kernel/blob/lpi4a/Makefile
-#   version = "5.10.113";
+# args of buildLinux:
+#   https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/linux/kernel/generic.nix
+buildLinux (args // {
+  # check version in kernel source tree's Makefile
+  #   https://github.com/revyos/thead-kernel/blob/lpi4a/Makefile
+  version = "5.10.113";
 
-#   # according to https://wiki.sipeed.com/hardware/zh/lichee/th1520/lpi4a/7_develop_revyos.html
-#   defconfig = "revyos_defconfig";
+  # according to https://wiki.sipeed.com/hardware/zh/lichee/th1520/lpi4a/7_develop_revyos.html
+  defconfig = "light_config";
 
-#   structuredExtraConfig = with lib.kernel; {
-#     MEDIA_DIGITAL_TV_SUPPORT = no;
-#   };
-# }) // (args.argsOverride or {})
+  structuredExtraConfig = with lib.kernel; {
+    MEDIA_DIGITAL_TV_SUPPORT = no;
+  };
+}) // (args.argsOverride or {})
 
 
-{
-  lib,
-  src,
-  linuxManualConfig,
-  ubootTools,
-  ...
-}:
-(linuxManualConfig {
-  version = "5.10.113-thead-1520";
-  modDirVersion = "5.10.113";
+# {
+#   lib,
+#   src,
+#   linuxManualConfig,
+#   ubootTools,
+#   ...
+# }:
+# (linuxManualConfig {
+#   version = "5.10.113-thead-1520";
+#   modDirVersion = "5.10.113";
 
-  inherit src;
+#   inherit src;
   
-  # path to the kernel config file
-  configfile = ./light_config;
+#   # path to the kernel config file
+#   configfile = ./light_config;
 
-  extraMeta.branch = "lp4a";
+#   extraMeta.branch = "lp4a";
 
-  allowImportFromDerivation = true;
-})
+#   allowImportFromDerivation = true;
+# })
