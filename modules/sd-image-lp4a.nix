bad89d2edd16d30582749aeafc0933050b991906
Ryan Yin <xiaoyin_c@qq.com>
2023-06-26 08:25:58 +0800

    fix: build sdImage

--- modules/sd-image-lp4a.nix
+++ modules/sd-image-lp4a.nix
@@ -1,67 +1,68 @@
 # method 1, use "${modulesPath}/installer/sd-card/sd-image.nix"
 #  with this method, the generated image has only /boot and /nix/store... I don't know if it works.
 #
-# { config, modulesPath, pkgs, ... }: 
-# let
-#   light_aon_fpga = (pkgs.callPackage ../pkgs/firmware/light_aon_fpga.nix {});
-#   light_c906_audio = (pkgs.callPackage ../pkgs/firmware/light_c906_audio.nix {});
-# in {
-#   # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/sd-card/sd-image.nix
-#   imports = [
-#     "${modulesPath}/installer/sd-card/sd-image.nix"
-#   ];
-
-#   fileSystems = {
-#     "/" = {
-#       device = "/dev/disk/by-label/NIXOS_SD";
-#       fsType = "ext4";
-#       options = ["noatime"];
-#     };
-#   };
-
-#   sdImage = {
-#     imageBaseName = "nixos-licheepi4a-sd-image";
-#     # for local usage, do not need to compress it.
-#     compressImage = false;
-#     populateFirmwareCommands = '''';
-
-#     # generate /boot
-#     populateRootCommands = ''
-#       mkdir -p ./files/boot
-#       ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
-
-#       cp ${pkgs.opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin ./files/boot
-#       cp ${light_aon_fpga}/lib/firmware/light_aon_fpga.bin ./files/boot
-#       cp ${light_c906_audio}/lib/firmware/light_c906_audio.bin ./files/boot
-#     '';
-#   };
-# }
-
-
-# method 2, use "${modulesPath}/../lib/make-disk-image.nix"
-# with this method, the generated image has a basic fhs layout.
-#
-{ config, lib, nixpkgs, modulesPath, pkgs, ... }: {
+{ config, modulesPath, pkgs, ... }: 
+let
+  light_aon_fpga = (pkgs.callPackage ../pkgs/firmware/light_aon_fpga.nix {});
+  light_c906_audio = (pkgs.callPackage ../pkgs/firmware/light_c906_audio.nix {});
+in {
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/installer/sd-card/sd-image.nix
+  imports = [
+    "${modulesPath}/installer/sd-card/sd-image.nix"
+  ];
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-label/nixos";
+      device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
       options = ["noatime"];
     };
   };
 
-  # https://github.com/NixOS/nixpkgs/blob/master/nixos/lib/make-disk-image.nix
-  system.build.sdImage = import "${modulesPath}/../lib/make-disk-image.nix" {
-    inherit config lib;
-    # need to use the unmodified corss build system to build the image for riscv64
-    pkgs = import nixpkgs {
-      localSystem = "x86_64-linux";
-      corssSystem = "riscv64-linux";
-    };
-    name = "licheepi4a-sd-image";
-    copyChannel = false;
-    format = "raw";
-  };
+  sdImage = {
+    imageBaseName = "nixos-licheepi4a-sd-image";
+    # for local usage, do not need to compress it.
+    compressImage = false;
+    populateFirmwareCommands = '''';
 
+    # generate /boot
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+      ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
+
+      cp ${pkgs.opensbi}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin ./files/boot
+      cp ${light_aon_fpga}/lib/firmware/light_aon_fpga.bin ./files/boot
+      cp ${light_c906_audio}/lib/firmware/light_c906_audio.bin ./files/boot
+    '';
+  };
 }
+
+
+# method 2, use "${modulesPath}/../lib/make-disk-image.nix"
+# with this method, the generated image has a basic fhs layout.
+# BUT THIS METHOD DOES NOT WORK, some seabios releated error(it seems do not support riscv64)
+#
+# { config, lib, nixpkgs, modulesPath, pkgs, ... }: {
+
+#   fileSystems = {
+#     "/" = {
+#       device = "/dev/disk/by-label/nixos";
+#       fsType = "ext4";
+#       options = ["noatime"];
+#     };
+#   };
+
+#   # https://github.com/NixOS/nixpkgs/blob/master/nixos/lib/make-disk-image.nix
+#   system.build.sdImage = import "${modulesPath}/../lib/make-disk-image.nix" {
+#     inherit config lib;
+#     # need to use the unmodified corss build system to build the image for riscv64
+#     pkgs = import nixpkgs {
+#       localSystem = "x86_64-linux";
+#       corssSystem = "riscv64-linux";
+#     };
+#     name = "licheepi4a-sd-image";
+#     copyChannel = false;
+#     format = "raw";
+#   };
+
+# }
