8c2e8fd385b3a8d51d0e687895cf6b6a424f15d1
Ryan Yin <xiaoyin_c@qq.com>
2023-08-06 19:39:15 +0800

    docs: update opensbi's build error

--- pkgs/opensbi/default.nix
+++ pkgs/opensbi/default.nix
@@ -19,16 +19,21 @@ stdenv.mkDerivation {
 # Build OpenSBI from source.
 # Currently failed with error:
 #        last 10 log lines:
-#        >  CC        lib/sbi/sbi_misaligned_ldst.o
-#        >  CC        lib/sbi/sbi_platform.o
-#        >  CC        lib/sbi/sbi_scratch.o
-#        >  CC        lib/sbi/sbi_string.o
-#        >  CC        lib/sbi/sbi_system.o
-#        >  CC        lib/sbi/sbi_timer.o
-#        >  CC        lib/sbi/sbi_tlb.o
-#        > /build/source/lib/sbi/sbi_tlb.c: Assembler messages:
-#        > /build/source/lib/sbi/sbi_tlb.c:187: Error: unrecognized opcode `fence.i', extension `zifencei' required
-#        > make: *** [Makefile:339: /build/source/build/lib/sbi/sbi_tlb.o] Error 1
+#        > /build/source/lib/utils/libfdt/fdt_wip.c:55: undefined reference to `__stack_chk_fail'
+#        > /nix/store/sik6wrw04rlll76ab55f36wi1vi4kmic-riscv64-unknown-linux-gnu-binutils-2.40/bin/riscv64-unknown-linux-gnu-ld: /build/source/build/platform/generic/lib/libplatsbi.a
+# (fdt_wip.o): in function `fdt_nop_property':
+#        > /build/source/lib/utils/libfdt/fdt_wip.c:71: undefined reference to `__stack_chk_guard'
+#        > /nix/store/sik6wrw04rlll76ab55f36wi1vi4kmic-riscv64-unknown-linux-gnu-binutils-2.40/bin/riscv64-unknown-linux-gnu-ld: /build/source/build/platform/generic/lib/libplatsbi.a
+# (fdt_wip.o): in function `fdt_node_end_offset_':
+#        > /build/source/lib/utils/libfdt/fdt_wip.c:78: undefined reference to `__stack_chk_fail'
+#        > /nix/store/sik6wrw04rlll76ab55f36wi1vi4kmic-riscv64-unknown-linux-gnu-binutils-2.40/bin/riscv64-unknown-linux-gnu-ld: /build/source/lib/utils/libfdt/fdt_wip.c:81: undefine
+# d reference to `__stack_chk_guard'
+#        > /nix/store/sik6wrw04rlll76ab55f36wi1vi4kmic-riscv64-unknown-linux-gnu-binutils-2.40/bin/riscv64-unknown-linux-gnu-ld: /build/source/build/platform/generic/lib/libplatsbi.a
+# (fdt_wip.o): in function `fdt_nop_region_':
+#        > /build/source/lib/utils/libfdt/fdt_wip.c:55: undefined reference to `__stack_chk_fail'
+#        > collect2: error: ld returned 1 exit status
+#        > make: *** [Makefile:358: /build/source/build/platform/generic/firmware/fw_dynamic.elf] Error 1
+#        For full logs, run 'nix log /nix/store/x12gq0y029w719l23vhzj20336qxq0y3-opensbi-riscv64-unknown-linux-gnu-1.2.drv'.
 # 
 # https://github.com/NixOS/nixpkgs/blob/32deda9ec08f550e4e0ece7708c4b674b2ca0dda/pkgs/misc/opensbi/default.nix#L11
 # https://github.com/chainsx/fedora-riscv-builder/blob/20230623-0255/build.sh
@@ -37,7 +42,8 @@ stdenv.mkDerivation {
 #     owner = "revyos";
 #     repo = "thead-opensbi";
 #     rev = "lpi4a";
-#     sha256 = "";
+#     sha256 = "sha256-MgOlxvo2spe/TJHUbyymy5W/zJq6CLdf4iuyQRVNeU4=";
+#     # sha256 = "";
 #   };
 #   makeFlags = pkgs.opensbi.makeFlags ++ ["FW_PIC=y"];
 # })
